61b22863237eaed89ecb3efe5d3bf13a589e6208
sjdirect <sjdirect@gmail.com>
2015-10-09 13:58:00 -0700

    Took a few mins to get tests to run. Referenced unofficial nuget packages for easier setup/deployment. Had to change from [SetUp] to [TestFixtureSetUp] since creating and disposing each instance was causing issues.

--- src/Abot.Plugins.Awesomium.Test/AwesomiumPageRequesterTest.cs
+++ src/Abot.Plugins.Awesomium.Test/AwesomiumPageRequesterTest.cs
@@ -8,48 +8,47 @@ using NUnit.Framework;
 
 namespace Abot.Plugins.Awesomium.Test
 {
-   [TestFixture]
-   public class AwesomiumPageRequesterTest
-   {
-      private DirectoryInfo _buildDir;
-      private IPageRequester _requester;
+    [TestFixture]
+    public class AwesomiumPageRequesterTest
+    {
+        private DirectoryInfo _buildDir;
+        private IPageRequester _requester;
 
-      [SetUp]
-      public void SetUp()
-      {
-         _requester = new AwesomiumPageRequester(BuildDir);
-      }
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _requester = new AwesomiumPageRequester(BuildDir);
+        }
 
-      [TearDown]
-      public void TearDown()
-      {
-         _requester.Dispose();
-      }
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            _requester.Dispose();
+        }
 
-      [Test]
-      public void DownloadPage_Google_Downloads()
-      {
-         CrawledPage page = _requester.MakeRequest(new Uri("http://google.co.uk"));
+        [Test]
+        public void DownloadPage_Google_Downloads()
+        {
+            CrawledPage page = _requester.MakeRequest(new Uri("http://google.co.uk"));
 
-         Assert.IsNotNull(page);
-      }
+            Assert.IsNotNull(page);
+        }
 
-      [Test]
-      public void DownloadPage_ThreeOneAfterAnother_Downloads()
-      {
-         _requester.MakeRequest(new Uri("http://bbc.co.uk"));
-         _requester.MakeRequest(new Uri("http://lookers.co.uk"));
-         _requester.MakeRequest(new Uri("http://autotrader.co.uk"));
-      }
+        [Test]
+        public void DownloadPage_ThreeOneAfterAnother_Downloads()
+        {
+            var page1 = _requester.MakeRequest(new Uri("http://bbc.co.uk"));
+            var page2 = _requester.MakeRequest(new Uri("http://lookers.co.uk"));
+            var page3 = _requester.MakeRequest(new Uri("http://autotrader.co.uk"));
+        }
 
-
-      protected DirectoryInfo BuildDir
-      {
-         get
-         {
-            return _buildDir ??
-                   (_buildDir = new FileInfo(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath).Directory);
-         }
-      }
-   }
+        protected DirectoryInfo BuildDir
+        {
+            get
+            {
+                return _buildDir ??
+                       (_buildDir = new FileInfo(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath).Directory);
+            }
+        }
+    }
 }
